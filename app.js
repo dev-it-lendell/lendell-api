4864e48cc215516e639aeb0e5b3be5f3ee2a5391
Bernard Gresola <btgresola@uerm.edu.ph>
2025-07-31 15:53:27 +0800

    Update app.js

--- app.js
+++ app.js
@@ -42,8 +42,9 @@ const PORT =
   app.use(morgan("dev"));
 
   app.set("trust proxy", 1);
-  app.use(bodyParser.json({ limit: "50mb" }));
-  app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+  app.use(bodyParser.json({ limit: "150mb" }));
+  app.use(bodyParser.urlencoded({ limit: "150mb", extended: true }));
+  
   app.use(express.json());
   app.use(cookieParser());
   app.use(cors(corsOptions));
